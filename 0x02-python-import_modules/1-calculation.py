bc89f3e66a0d382e4836213e9351692893671239
a11y-2824 <wachana.anne@gmail.com>
2022-09-22 18:37:31 -0700

    import function and perform calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,21 +1,9 @@
 #!usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1.py import add
+    from calculator_1.py import add, sub, mul, div
     a = 10
     b = 5
     print("() + () = ()".format(a, b, add(a, b)))
-
-    from calculator_1.py import sub
-    a = 10
-    b = 5
     print("{} - {} = {}".format(a, b, sub(a, b)))
-
-    from calculator_1.py import mul
-    a = 10
-    b = 5
     print("{} * {} = {}".format(a, b, mul(a, b)))
-
-    from calculator_1.py import div
-    a = 10
-    b = 5
     print("{} / {} = {}".format(a, b, div(a, b)))
