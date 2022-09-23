d2ef34143a28c62f3a1e9c9bac58297076121ad2
a11y-2824 <wachana.anne@gmail.com>
2022-09-22 18:00:51 -0700

    importing functions

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   from add_0.py import add
-   a = 1
-   b = 2
-   print("a + b = add(a, b) value")
+    from add_0.py import add
+    a = 1
+    b = 2
+    print("a + b = add(a, b)")
