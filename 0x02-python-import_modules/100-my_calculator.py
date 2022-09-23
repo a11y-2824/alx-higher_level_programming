f77122a3e88a5239730f754cd69c4b8397cf51e5
a11y-2824 <wachana.anne@gmail.com>
2022-09-23 01:48:46 -0700

    print statement

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-if __name == "__main__":
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    int a
-    int b
-    print(<a> <operator> <b> = <result>.format(a, operator, b, result)
-            return(1)
+    a = int()
+    b = int()
+
+if op == '+':
+    print("{} + {} = {}".format(a, b, add(a, b)))
+elif op == '-':
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+elif op == '*':
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+elif op == '/':
+    print("{} / {} = {}".format(a, b, div(a, b)))
