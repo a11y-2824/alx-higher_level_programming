b5cc1f486317cfbade1628b786a629676f2fb974
a11y-2824 <wachana.anne@gmail.com>
2022-09-22 19:52:18 -0700

    imports

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name == "__main__":
+    from calculator_1 import add, sub, mul, div
+    int a
+    int b
+    print(<a> <operator> <b> = <result>.format(a, operator, b, result)
+            return(1)
