7b39c5a85e4f95f6b5c31f5b36c201894c18cd8e
a11y-2824 <wachana.anne@gmail.com>
2022-10-17 02:23:29 +0300

    update missing files

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+for i in range(48, 58):
+    print('{:d}'.format(map(hex)))
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' Create a class '''
+
+
+class Square:
+    ''' Define attributes'''
+    def __init__(self, size):
+        '''assign the instance'''
+        self.size = size
