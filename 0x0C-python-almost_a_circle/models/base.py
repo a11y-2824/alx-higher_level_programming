9889e9a42b85c2d2ae1c2eb5225309246ecfaeb9
a11y-2824 <wachana.anne@gmail.com>
2022-10-31 04:52:57 +0300

    add test file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-
-
 '''Create a class'''
 
 
-class Base
-'''create a private class attribute'''
-	def __init__(self, id=None):
-'''Create a public instance called id'''
-	__nb_objects = 0
-'''create a loop'''
-		if id != None:
-			self.id = id
-		else:
-			__nb_objects = ++
-			sel.id = __nb_objects
+class Base:
+    '''create a private class attribute'''
 
+    def __init__(self, id=None):
+        '''Create a public instance called id'''
+        __nb_objects = 0
+    '''create a loop'''
+    if id is not None:
+        self.id = id
+    else:
+        __nb_objects = ++
+        sel.id = __nb_objects
