866fb9eac3a4e265f7a30a760f22e40e3ed8c70c
a11y-2824 <wachana.anne@gmail.com>
2022-10-31 04:20:32 +0300

    add a new class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+'''Create a class'''
+
+
+class Base
+'''create a private class attribute'''
+	def __init__(self, id=None):
+'''Create a public instance called id'''
+	__nb_objects = 0
+'''create a loop'''
+		if id != None:
+			self.id = id
+		else:
+			__nb_objects = ++
+			sel.id = __nb_objects
+
