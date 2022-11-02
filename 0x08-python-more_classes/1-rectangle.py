d0a5d2d19e70e4012bfbef92da8e7b05909d0c18
a11y-2824 <wachana.anne@gmail.com>
2022-11-02 05:02:20 +0300

    update missing files

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+'''write a class Rectangle'''
+''' Create a private property width '''
+''' Make width a property setter'''
+''' Raise a TypeError with message'''
+''' Raise a ValueError with message'''
+''' Create a private property height '''
+''' Make height1; a property setter'''
+''' Raise a TypeError with message'''
+''' Raise a ValueError with message'''
